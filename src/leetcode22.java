c30a66a3fbbe2fb26244a222bcc0e1a7221cfc5b
AhmedTitef <57777850+AhmedTitef@users.noreply.github.com>
2020-09-24 08:59:28 -0700

    master the coding interview exercise

---
+++ src/leetcode22.java
@@ -0,0 +1,50 @@
+public class leetcode22 {
+
+
+    public static void main(String[] args) {
+        leetcode22 leetcode22 = new leetcode22();
+        leetcode22.searchBST( new TreeNode( 0 ) );
+    }
+
+     class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+         this.right = right;
+      }
+  }
+
+
+        public TreeNode searchBST(TreeNode root, int val) {
+            TreeNode currentNode = root;
+
+             while (true){
+                 if (val < currentNode.val){
+                   currentNode = currentNode.left;
+
+                 }else if (val > currentNode.val){
+                     currentNode = currentNode.right;
+
+
+                 }else if (val  == currentNode.val){
+                     return currentNode;
+                 }
+                 else {
+                     return null;
+                 }
+
+
+         }
+
+
+
+
+
+    }
+
+}
